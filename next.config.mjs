6fe75472223de462e111e367c4b352aca79a0b6a
mac <vabustillos@gmail.com>
2026-08-28 12:55:40 -0600

    Save uncommitted changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,22 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.mixkit.co",
+      },
+    ],
+  },
 }
 
 export default nextConfig
